25326bd00420f68187b8da88e4fc064c6b523c32
Marcelin-08 <marcelo.quaresma011@gmail.com>
2025-08-29 10:40:12 -0300

    termino do trabalho

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,48 @@
-
 public class ClasseAutomotores {
+    String corAt;
+    String marcaAt;
+    String modeloAt; 
+    String tipoAt;
 
+    public ClasseAutomotores() {
+    }
+
+    public ClasseAutomotores(String corAt, String marcaAt, String modeloAt, String tipoAt) {
+        this.corAt = corAt;
+        this.marcaAt = marcaAt;
+        this.modeloAt = modeloAt;
+        this.tipoAt = tipoAt;
+    }
+
+    public String getCorAt() {
+        return corAt;
+    }
+
+    public void setCorAt(String corAt) {
+        this.corAt = corAt;
+    }
+
+    public String getMarcaAt() {
+        return marcaAt;
+    }
+
+    public void setMarcaAt(String marcaAt) {
+        this.marcaAt = marcaAt;
+    }
+
+    public String getModeloAt() {
+        return modeloAt;
+    }
+
+    public void setModeloAt(String modeloAt) {
+        this.modeloAt = modeloAt;
+    }
+
+    public String getTipoAt() {
+        return tipoAt;
+    }
+
+    public void setTipoAt(String tipoAt) {
+        this.tipoAt = tipoAt;
+    }
 }
---
+++ src/ClasseCarros.java
@@ -0,0 +1,10 @@
+public class ClasseCarros extends ClasseAutomotores {
+
+    public ClasseCarros() {
+        super();
+    }
+
+    public ClasseCarros(String corAt, String marcaAt, String modeloAt, String tipoAt) {
+        super(corAt, marcaAt, modeloAt, tipoAt);
+    }
+}
