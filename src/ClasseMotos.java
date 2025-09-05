307d3851254d17efd8f2eb2767a9c31548c66c46
Marcelin-08 <marcelo.quaresma011@gmail.com>
2025-09-05 10:29:09 -0300

    Adicionada classe motos herdando atributos e métodos da classe

---
+++ src/ClasseMotos.java
@@ -0,0 +1,11 @@
+public class ClasseMotos extends ClasseAutomotores {
+
+	 public ClasseMotos() {
+	        super();
+	    }
+
+    public ClasseMotos(String corAt, String marcaAt, String modeloAt, String tipoAt) {
+        super(corAt, marcaAt, modeloAt, tipoAt);
+    }
+
+}
